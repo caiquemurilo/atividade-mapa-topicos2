0a2395834746a24917f41b23eab78315b09e56df
Caique Murilo Sacramento <caiquemurilo@gmail.com>
2024-06-30 22:57:15 -0300

    criacao de nova branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Olá, Mundo!");
+    System.out.println("alteracoes para merge da nova branch");
+
   }
 }
